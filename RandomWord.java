6097dc4d61d396554d12b914befc18cf9914c7b0
JoannaXu <joannaxu985@outlook.com>
2025-10-18 22:33:10 +0100

    upload RandomWord.java

--- RandomWord.java
+++ RandomWord.java
@@ -4,8 +4,23 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     public static void main(String[] args) {
+        int i = 1;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            String value = StdIn.readString();
+            boolean flag = StdRandom.bernoulli(1.0 / i);
+            if (flag) {
+                champion = value;
+            }
+            i++;
+        }
+        StdOut.println(champion);
 
     }
 }
