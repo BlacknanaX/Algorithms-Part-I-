f8e382cd1da38e9b7371511f38eb6c2079ae37fd
JoannaXu <joannaxu985@outlook.com>
2025-10-19 00:39:03 +0100

    add command for RandomWord

--- RandomWord.java
+++ RandomWord.java
@@ -12,6 +12,8 @@ public class RandomWord {
     public static void main(String[] args) {
         int i = 1;
         String champion = "";
+        // loop with StdIn.isEmpty may happen no termination
+        // so try ctr+d at the end if run with command line
         while (!StdIn.isEmpty()) {
             String value = StdIn.readString();
             boolean flag = StdRandom.bernoulli(1.0 / i);
